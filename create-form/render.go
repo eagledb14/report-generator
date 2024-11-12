5be2d8e454769ce370cd7f9b5c5787930189554d
eagledb14 <eagledb14@gmail.com>
2024-11-12 10:47:00 -0500

    changed tar to zip, fixed char formatting for html

--- create-form/render.go
+++ create-form/render.go
@@ -8,7 +8,7 @@ func CreateHtml(md string, title string, amber bool) string {
 	md =  "<div class=\"content\">\n\n" + md + "</div>"
 	html := markdown.ToHTML([]byte(md), nil, nil)
 
-	file := "<!DOCTYPE html>" + "<head>" + banner(title, amber) + "</head>" + string(html)
+	file := "<!DOCTYPE html>" + "<head>\n<meta charset=\"UTF-8\">\n" + styles() + "\n</head>\n" + banner(title, amber) + string(html)
 	return file
 }
 
--- makefile
+++ makefile
@@ -4,7 +4,7 @@ build:
 	GOOS=linux GOARCH=amd64 go build -o form-scanner .
 	GOOS=windows GOARCH=amd64 go build -o form-scanner.exe .
 	-rm ./resources/event_cache.db
-	tar -czf form-scanner.tar.gz ./resources ./form-scanner*
+	zip -r form-scanner.zip ./resources ./form-scanner*
 	rm form-scanner
 	rm form-scanner.exe
 
